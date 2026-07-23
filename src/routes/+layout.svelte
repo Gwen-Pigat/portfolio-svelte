e4454c9a1bc8243229136ecd9f874b903344bc40
Gwen Pigat <pixofheaven@gmail.com>
2026-07-23 19:33:35 +0200

    fix(favicon): replace default SvelteKit favicon with Orizenh logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/images/favicon-96x96.png" />
 </svelte:head>
 
 {@render children()}
